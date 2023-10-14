845ab6bf94723abbfa4aab3fe476adc1ce477ecd
EgorovZaxar555 <101442263+EgorovZaxar555@users.noreply.github.com>
2023-10-14 22:35:00 +0700

    Add files via upload

---
+++ chess2.js
@@ -0,0 +1,8 @@
+for (var i=0;i<8;++i) {
+ document.write("");
+ for (var j=0;j<8;++j) document.write("<span onclick=\"hu("+j+","+i+")\" id=\""+j+i+"\"><img></span>");
+ document.write("<br>");
+}
+
+
+
